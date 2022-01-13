1738fa737beeae9cb3303cd3160ba871d05debc8
amassiro <massironi.andrea@gmail.com>
2022-01-13 14:19:19 +0100

    up

--- first_test.py
+++ first_test.py
@@ -1,4 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def f(t):
+    return np.exp(-t) * np.cos(2*np.pi*t)
 
 
 print ("test with a simple function")
+
+t1 = np.arange(0.0, 5.0, 0.01)
+
+plt.plot(t1, f(t1), "b .")
+
+plt.show()
+
+
+#
+# this is the target function
+#
+#
+
+
+
+
+
 
