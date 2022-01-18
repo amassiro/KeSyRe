bde90ec6c289eba501d5383c6d88df09f211311b
amassiro <massironi.andrea@gmail.com>
2022-01-18 14:49:48 +0100

    tests

---
+++ Core/KeSyRe.py
@@ -0,0 +1,32 @@
+#
+# KeSyre class
+#
+#
+#
+
+
+class classKeSyre():
+
+
+    def __init__(self):
+      
+      print ("constructor")
+      
+      
+    def setInputs(self, inputs):
+    
+      self._inputs = inputs
+
+
+    def defineFunctions(self, functions):
+    
+      # boh ... to be thought ...
+      
+      print ("boh")
+      
+      
+      
+      
+      
+
+      
---
+++ test/test_kesyre.py
@@ -0,0 +1,20 @@
+
+import sys
+sys.path.append(".")
+sys.path.append("../")
+#sys.path.append("Core")
+
+
+from Core.KeSyRe import classKeSyRe
+from Core import KeSyRe
+#from KeSyRe import classKeSyRe
+
+
+if __name__ == '__main__':
+  
+  myKeSyRe = classKeSyRe()
+
+
+
+
+
